cc703f61bec777157d074eac8324807ef37eebf1
LAPTOP-VPU5DPL7\prestodu <prestondu10231023@gmail.com>
2025-04-01 23:15:46 +0800

    update game state page

--- app/src/main/java/edu/cuhk/csci3310/basketball_app/MainActivity.java
+++ app/src/main/java/edu/cuhk/csci3310/basketball_app/MainActivity.java
@@ -1,6 +1,9 @@
 package edu.cuhk.csci3310.basketball_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,39 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnGameState, btnPlayerStats, btnCourtFinder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnGameState = findViewById(R.id.btnGameState);
+        btnPlayerStats = findViewById(R.id.btnPlayerStats);
+        btnCourtFinder = findViewById(R.id.btnCourtFinder);
+
+        btnGameState.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GameStateTrackerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnPlayerStats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PlayerStatisticsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnCourtFinder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CourtFinderActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
